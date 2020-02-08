e4a59ed6da75b500b29f544d74fc2fe4b9b28282
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2020-02-08 20:23:35 -0300

    Finishing of Main screen controller.

--- SortAlgorithmsTimer/src/controller/FXMLMainScreenController.java
+++ SortAlgorithmsTimer/src/controller/FXMLMainScreenController.java
@@ -11,6 +11,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import model.DataTypes;
 
 public class FXMLMainScreenController implements Initializable{
@@ -18,6 +20,8 @@ public class FXMLMainScreenController implements Initializable{
 	private Button runButton;
 	@FXML
     private ComboBox<DataTypes> cbDataType;
+	@FXML
+    private Spinner<Integer> vectorSize = new Spinner<Integer>();
 	private ArrayList<DataTypes> dataTypesList = new ArrayList<>();
 	private ObservableList<DataTypes> obsDataTypes;
 	
@@ -25,15 +29,16 @@ public class FXMLMainScreenController implements Initializable{
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		loadComboBox();
+		loadSpinner();
 	}
 	
 	@FXML
 	void runButtonEvent(ActionEvent event) {
 		DataTypes selectedDataType = cbDataType.getSelectionModel().getSelectedItem();
-		System.out.println("Id: " + selectedDataType.getId() + "Data type: " + selectedDataType.getDataType());
+		Integer size = vectorSize.getValue();
 	}
 	
-	public void loadComboBox() {
+	private void loadComboBox() {
 		DataTypes character = new DataTypes(1,"Character");
 		DataTypes doubleData = new DataTypes(2, "Double");
 		DataTypes integerData = new DataTypes(3, "Integer");
@@ -44,7 +49,11 @@ public class FXMLMainScreenController implements Initializable{
 		dataTypesList.add(stringData);
 		obsDataTypes =FXCollections.observableArrayList(dataTypesList);
 		cbDataType.setItems(obsDataTypes);
-		
+	}
+	private void loadSpinner() {
+		SpinnerValueFactory<Integer> valueFactory = //
+                new SpinnerValueFactory.IntegerSpinnerValueFactory(10, 1000000000, 10);
+		vectorSize.setValueFactory(valueFactory);
 	}
 	
 }
